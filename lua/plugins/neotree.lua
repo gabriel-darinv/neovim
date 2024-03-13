ec24f92bcc7bc74086b242636aa0f0e647c4c721
gabriel-darinv <gabrieldarin@hotmail.com>
2024-03-13 07:46:36 -0300

    Added neotree config to open at neovim start and changed icons

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,6 +8,37 @@ return {
     -- "3rd/image.nvim",    
   },
   config = function()
+    require("neo-tree").setup({  
+      default_component_configs = {
+        close_if_last_window = false,
+        popup_border_style = "rounded",
+        window = {
+          position = "left",
+          width = 30,
+        },
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "◇",
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added     = "⁅+⁆", -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified  = "M", -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted   = "×",-- this can only be used in the git_status source
+            renamed   = "R",-- this can only be used in the git_status source
+            -- Status type
+            untracked = "U", -- 
+            ignored   = "",-- 
+            unstaged  = "◯", -- µ⨆⨆∪⊔⊠⋄⋐⋱⋃↖↗↘↙→●◉◯◶ÚŮUi$₿£i⋮⋯⋱⊠⁅⁆*‼‽:§↥←◌₀∙∵∹≬⋆≈∷∸∘₀⁰√
+            staged    = "◉",
+            conflict  = "!",
+          }
+        },
+      }
+    })
+
     vim.keymap.set('n','<leader>e',':Neotree filesystem reveal left<CR>')
     vim.keymap.set('i','<C-e>','<ESC>:Neotree filesystem reveal left<CR>')
     --vim.keymap.set('n','<leader><leader>',':Neotree filesystem reveal float<CR>')
